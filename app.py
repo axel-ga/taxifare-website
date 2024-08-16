3a8ad31c58269cae6c16d5a117ed0ab66cdd5892
Axel <13357039+axel-ga@users.noreply.github.com>
2024-08-16 12:09:46 +0200

    md

--- app.py
+++ app.py
@@ -59,7 +59,29 @@ payload = {
 response = requests.get(url,params=payload).json()
 print(response.status_code)
 st.write(response, unsafe_allow_html=False)
+
 4. Let's retrieve the prediction from the **JSON** returned by the API...
 
 ## Finally, we can display the prediction to the user
 '''
+url = 'https://taxifare.lewagon.ai/predict'
+
+pickup_datetime = st.date_input("pickup date", value="default_value_today")
+pickup_longitude = st.number_input("Insert a pickup lon", value=None, placeholder="Type a number..."
+pickup_latitude = st.number_input("Insert a pickup lat", value=None, placeholder="Type a number..."
+dropoff_longitude = st.number_input("Insert a dropoff lon", value=None, placeholder="Type a number..."
+dropoff_latitude = st.number_input("Insert a droppoff lat", value=None, placeholder="Type a number..."
+passenger_count = st.number_input('passenger count', min_value=None, max_value=None)
+
+payload = {
+    'pickup_datetime':pickup_datetime,
+    'pickup_longitude':pickup_longitude,
+    'pickup_latitude':pickup_latitude,
+    'dropoff_longitude':dropoff_longitude,
+    'dropoff_latitude':dropoff_latitude,
+    'passenger_count':passenger_count
+    }
+
+response = requests.get(url,params=payload).json()
+print(response.status_code)
+st.write(response, unsafe_allow_html=False)
