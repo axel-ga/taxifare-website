deaad1c99daad817dcd75a2cd237bb65ec3f4a2c
Axel <13357039+axel-ga@users.noreply.github.com>
2024-08-16 12:10:29 +0200

    fixe typo

--- app.py
+++ app.py
@@ -67,10 +67,10 @@ st.write(response, unsafe_allow_html=False)
 url = 'https://taxifare.lewagon.ai/predict'
 
 pickup_datetime = st.date_input("pickup date", value="default_value_today")
-pickup_longitude = st.number_input("Insert a pickup lon", value=None, placeholder="Type a number..."
-pickup_latitude = st.number_input("Insert a pickup lat", value=None, placeholder="Type a number..."
-dropoff_longitude = st.number_input("Insert a dropoff lon", value=None, placeholder="Type a number..."
-dropoff_latitude = st.number_input("Insert a droppoff lat", value=None, placeholder="Type a number..."
+pickup_longitude = st.number_input("Insert a pickup lon", value=None, placeholder="Type a number...")
+pickup_latitude = st.number_input("Insert a pickup lat", value=None, placeholder="Type a number...")
+dropoff_longitude = st.number_input("Insert a dropoff lon", value=None, placeholder="Type a number...")
+dropoff_latitude = st.number_input("Insert a droppoff lat", value=None, placeholder="Type a number...")
 passenger_count = st.number_input('passenger count', min_value=None, max_value=None)
 
 payload = {
